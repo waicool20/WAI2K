6ee8d052863d7c0d09683e61d60e18d8e38c109d
waicool20 <waicool20@gmail.com>
2018-11-08 19:56:11 +0800

    Implement valid dragging doll scanning for doll 1

--- src/main/kotlin/com/waicool20/wai2k/script/modules/CombatModule.kt
+++ src/main/kotlin/com/waicool20/wai2k/script/modules/CombatModule.kt
@@ -26,9 +26,16 @@ import com.waicool20.wai2k.game.DollType
 import com.waicool20.wai2k.game.LocationId
 import com.waicool20.wai2k.script.Navigator
 import com.waicool20.wai2k.script.ScriptRunner
+import com.waicool20.wai2k.util.Ocr
+import com.waicool20.wai2k.util.doOCRAndTrim
+import com.waicool20.waicoolutils.*
 import com.waicool20.waicoolutils.logging.loggerFor
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.yield
+import java.awt.Color
+import java.awt.image.BufferedImage
+
+private const val OCR_THRESHOLD = 2
 
 class CombatModule(
         scriptRunner: ScriptRunner,
@@ -38,6 +45,7 @@ class CombatModule(
         navigator: Navigator
 ) : ScriptModule(scriptRunner, region, config, profile, navigator) {
     private val logger = loggerFor<CombatModule>()
+
     override suspend fun execute() {
         if (!profile.combat.enabled) return
         switchDolls()
@@ -50,8 +58,12 @@ class CombatModule(
         region.subRegion(612, 167, 263, 667).clickRandomly()
         delay(100)
         applyFilters(1)
+        scanValidDolls(1).shuffled().first().clickRandomly()
     }
 
+    /**
+     * Applies the filters ( stars and types ) in formation doll list
+     */
     private suspend fun applyFilters(doll: Int) {
         logger.info("Applying doll filters for dragging doll $doll")
         val stars: Int
@@ -86,5 +98,59 @@ class CombatModule(
             logger.info("Confirming filters")
             find("$prefix/confirm.png").clickRandomly(); delay(100)
         }
+    }
+
+    /**
+     * Scans for valid dolls in the formation doll list
+     *
+     * @return List of regions that can be clicked to select the valid doll
+     */
+    private suspend fun scanValidDolls(doll: Int): List<AndroidRegion> {
+        logger.info("Scanning for valid dolls in filtered list for dragging doll $doll")
+        val name: String
+        val level: Int
+        with(profile.combat) {
+            when (doll) {
+                1 -> {
+                    name = doll1Name
+                    level = doll1Level
+                }
+                2 -> {
+                    name = doll2Name
+                    level = doll2Level
+                }
+                else -> error("Invalid doll: $doll")
+            }
+        }
+        // Temporary convenience class for storing doll regions
+        class DollRegions(val nameRegion: BufferedImage, val levelRegion: BufferedImage, val clickRegion: AndroidRegion)
+        // Optimize by taking a single screenshot and working on that
+        val image = region.takeScreenshot()
+        return region.findAllOrEmpty("combat/formation/lock.png")
+                // Transform the lock region into 3 distinct regions used for ocr and clicking by offset
+                .map {
+                    DollRegions(
+                            image.getSubimage(it.x + 67, it.y + 72, 161, 52),
+                            image.getSubimage(it.x + 183, it.y + 124, 45, 32),
+                            region.subRegion(it.x - 7, it.y, 244, 164)
+                    )
+                }
+                // Filter by name
+                .filterAsync(this) { Ocr.forConfig(config).doOCRAndTrim(it.nameRegion).distanceTo(name, Ocr.OCR_DISTANCE_MAP) < OCR_THRESHOLD }
+                // Filter by level
+                .filterAsync(this) {
+                    it.levelRegion.binarizeImage().scale(2.0).pad(20, 10, Color.WHITE).let { bi ->
+                        Ocr.forConfig(config, digitsOnly = true).doOCRAndTrim(bi).toIntOrNull() ?: 0 > level
+                    }
+                }
+                // Return click regions
+                .map { it.clickRegion }
+                .also {
+                    if (it.isEmpty()) {
+                        error("Failed to find dragging doll $doll that matches criteria")
+                    } else {
+                        logger.info("Found ${it.size} dolls that match the criteria for doll $doll")
+                    }
+                }
     }
 }
