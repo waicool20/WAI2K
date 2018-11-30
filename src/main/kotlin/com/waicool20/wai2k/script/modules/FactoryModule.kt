1a5459e3012974bbb793cd5b255e7b6c4aaa2dd3
waicool20 <waicool20@gmail.com>
2018-11-30 23:08:56 +0800

    Don't run disassemble after enhancement if there is space left

--- src/main/kotlin/com/waicool20/wai2k/script/modules/FactoryModule.kt
+++ src/main/kotlin/com/waicool20/wai2k/script/modules/FactoryModule.kt
@@ -50,6 +50,7 @@ class FactoryModule(
     private suspend fun checkDollOverflow() {
         if (!gameState.dollOverflow) return
         if (profile.factory.enhancement.enabled) enhanceDolls()
+        if (!gameState.dollOverflow) return
         if (profile.factory.disassembly.enabled) disassembleDolls()
     }
 
