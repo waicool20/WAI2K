13ba87de637b35b0fdeb60fc85cade2f79f75245
waicool20 <waicool20@gmail.com>
2019-04-20 02:43:04 +0800

    InitModule: Fix logistics support ocr error
    - Hyphen sometimes reads as some other character

--- src/main/kotlin/com/waicool20/wai2k/script/modules/InitModule.kt
+++ src/main/kotlin/com/waicool20/wai2k/script/modules/InitModule.kt
@@ -92,8 +92,9 @@ class InitModule(
                     )
                 }
                 .map { "${it[0].await()} ${it[1].await()} ${it[2].await()}" }
+                .also { println(it) }
                 .mapNotNull {
-                    Regex("(\\d) (\\d)\\s?-\\s?(\\d) (\\d\\d):(\\d\\d):(\\d\\d)").matchEntire(it)?.destructured
+                    Regex("(\\d) (\\d)\\s?[-—]\\s?(\\d) (\\d\\d):(\\d\\d):(\\d\\d)").matchEntire(it)?.destructured
                 }
         // Clear existing timers
         gameState.echelons.forEach { it.logisticsSupportAssignment = null }
