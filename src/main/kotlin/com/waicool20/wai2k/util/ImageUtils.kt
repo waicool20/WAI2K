a3f54450b62aac3ab3d99de4f74c981a092d3b3b
waicool20 <waicool20@gmail.com>
2020-01-15 23:26:45 +0800

    ImageUtils: Tweak blue node hue range

--- src/main/kotlin/com/waicool20/wai2k/util/ImageUtils.kt
+++ src/main/kotlin/com/waicool20/wai2k/util/ImageUtils.kt
@@ -32,7 +32,7 @@ import java.awt.image.BufferedImage
 fun BufferedImage.extractNodes(includeWhite: Boolean = true): GrayF32 {
     val hsv = asPlanar().asHsv()
     val redNodes = hsv.clone().apply { hsvFilter(hueRange = arrayOf(0..10, 350..360), satRange = arrayOf(20..100)) }.getBand(2)
-    val blueNodes = hsv.apply { hsvFilter(hueRange = 200..210, satRange = 20..100) }.getBand(2)
+    val blueNodes = hsv.apply { hsvFilter(hueRange = 190..210, satRange = 20..100) }.getBand(2)
     return if (includeWhite) {
         val whiteNodes = hsv.clone().apply { hsvFilter(satRange = 0..1, valRange = 210..255) }.getBand(2)
         whiteNodes + redNodes + blueNodes
