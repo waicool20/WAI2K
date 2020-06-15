2a3bed12d1745ea7116c4be36dc40116b0b1099a
FatDog <djk.kleyn@gmail.com>
2020-06-15 11:54:26 +1000

    Absolute coordinates for 0-2

---
+++ src/main/kotlin/com/waicool20/wai2k/script/modules/combat/maps/Map0-2-EX.kt
@@ -0,0 +1,89 @@
+/*
+ * GPLv3 License
+ *
+ *  Copyright (c) WAI2K by waicool20
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.waicool20.wai2k.script.modules.combat.maps
+
+import com.waicool20.cvauto.android.AndroidRegion
+import com.waicool20.wai2k.config.Wai2KConfig
+import com.waicool20.wai2k.config.Wai2KProfile
+import com.waicool20.wai2k.script.ScriptRunner
+import com.waicool20.wai2k.script.modules.combat.MapRunner
+import com.waicool20.waicoolutils.logging.loggerFor
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.yield
+import kotlin.random.Random
+
+class Map0_2_EX(
+        scriptRunner: ScriptRunner,
+        region: AndroidRegion,
+        config: Wai2KConfig,
+        profile: Wai2KProfile
+) : MapRunner(scriptRunner, region, config, profile) {
+    private val logger = loggerFor<Map0_2_EX>()
+    override val isCorpseDraggingMap = true
+    override val extractBlueNodes = false
+    override val extractYellowNodes = false
+
+    override suspend fun execute() {
+        // Bounce back for all nodes mostly on screen at once
+        if (gameState.requiresMapInit) {
+            logger.info("Zoom out")
+            region.pinch(
+                        Random.nextInt(900, 1000),
+                        Random.nextInt(300, 400),
+                        0.0,
+                        500
+            )
+            gameState.requiresMapInit = false
+            }
+        else{
+            region.pinch(
+                    Random.nextInt(444, 555),
+                    Random.nextInt(222, 333),
+                    0.0,
+                    300
+            )
+            }
+        delay(800) //Wait to settle
+        val rEchelons = deployEchelons(nodes[0], nodes[1])
+        mapRunnerRegions.startOperation.click(); yield()
+        waitForGNKSplash()
+        resupplyEchelons(rEchelons + nodes[1])
+        planPath()
+        waitForTurnEnd(5)
+        handleBattleResults()
+    }
+
+    private suspend fun planPath() {
+        logger.info("Selecting ehcelon at ${nodes[0]}")
+        nodes[0].findRegion().click()
+
+        logger.info("Entering planning mode")
+        mapRunnerRegions.planningMode.click(); yield()
+
+        logger.info("Selecting ${nodes[2]}")
+        nodes[2].findRegion().click()
+
+        logger.info("Selecting ${nodes[3]}")
+        nodes[3].findRegion().click(); yield()
+
+        logger.info("Executing plan")
+        mapRunnerRegions.executePlan.click()
+    }
+}
