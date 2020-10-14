5ec6b1098a31e45cf0f3084bfd34145503d4df98
waicool20 <waicool20@gmail.com>
2020-10-14 23:29:45 +0800

    HomographyMapRunner: Set retry to 5 times at most

--- src/main/kotlin/com/waicool20/wai2k/script/modules/combat/HomographyMapRunner.kt
+++ src/main/kotlin/com/waicool20/wai2k/script/modules/combat/HomographyMapRunner.kt
@@ -29,15 +29,13 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import com.waicool20.cvauto.android.AndroidRegion
 import com.waicool20.cvauto.util.transformRect
 import com.waicool20.wai2k.game.MapRunnerRegions
+import com.waicool20.wai2k.script.NodeNotFoundException
 import com.waicool20.wai2k.script.ScriptComponent
 import com.waicool20.wai2k.util.ai.MatchingModel
 import com.waicool20.wai2k.util.ai.MatchingTranslator
 import com.waicool20.waicoolutils.logging.loggerFor
 import georegression.struct.homography.Homography2D_F64
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import java.nio.file.Files
 import kotlin.math.max
 import kotlin.math.pow
@@ -131,7 +129,7 @@ abstract class HomographyMapRunner(scriptComponent: ScriptComponent) : MapRunner
     override suspend fun MapNode.findRegion(): AndroidRegion {
         val window = mapRunnerRegions.window
 
-        suspend fun retry(): AndroidRegion {
+        suspend fun resetView() {
             logger.info("Zoom out")
             region.pinch(
                 Random.nextInt(500, 700),
@@ -141,91 +139,97 @@ abstract class HomographyMapRunner(scriptComponent: ScriptComponent) : MapRunner
             )
             delay((900 * gameState.delayCoefficient).roundToLong()) //Wait to settle
             mapH = null
-            return findRegion()
         }
 
-        val h = mapH ?: try {
-            logger.info("Finding map transformation")
-            val prediction = predictor.predict(fullMap to ImageFactory.getInstance().fromImage(window.capture()))
-            logger.debug("Homography prediction metrics:")
-            logger.debug("Preprocess: ${metrics.latestMetric("Preprocess").value.toLong() / 1000000} ms")
-            logger.debug("Inference: ${metrics.latestMetric("Inference").value.toLong() / 1000000} ms")
-            logger.debug("Postprocess: ${metrics.latestMetric("Postprocess").value.toLong() / 1000000} ms")
-            logger.debug("Total: ${metrics.latestMetric("Total").value.toLong() / 1000000} ms")
-            mapH = prediction
-            prediction
-        } catch (e: TranslateException) {
-            return retry()
-        }
+        for (i in 0 until 5) {
+            val h = mapH ?: try {
+                logger.info("Finding map transformation")
+                val prediction = predictor.predict(fullMap to ImageFactory.getInstance().fromImage(window.capture()))
+                logger.debug("Homography prediction metrics:")
+                logger.debug("Preprocess: ${metrics.latestMetric("Preprocess").value.toLong() / 1000000} ms")
+                logger.debug("Inference: ${metrics.latestMetric("Inference").value.toLong() / 1000000} ms")
+                logger.debug("Postprocess: ${metrics.latestMetric("Postprocess").value.toLong() / 1000000} ms")
+                logger.debug("Total: ${metrics.latestMetric("Total").value.toLong() / 1000000} ms")
+                mapH = prediction
+                prediction
+            } catch (e: TranslateException) {
+                resetView()
+                continue
+            }
 
-        // Rect that is relative to the window
-        val rect = h.transformRect(rect)
-        logger.debug("$this estimated to be at Rect(x=${rect.x}, y=${rect.y}, width=${rect.width}, height=${rect.height})")
-        if (rect.width <= 0 || rect.height <= 0) {
-            logger.debug("Estimate failed basic dimension test, will retry")
-            return retry()
-        }
-        val roi = window.copyAs<AndroidRegion>(
-            window.x + rect.x,
-            window.y + rect.y,
-            rect.width,
-            rect.height
-        )
-        val mapDiff = (rect.width.toDouble() - width).pow(2) + (rect.height.toDouble() - height).pow(2)
-        if (mapDiff > maxMapDiff.pow(2)) {
-            logger.info("Estimate failed map difference test, will retry | diff=$mapDiff, max=$maxMapDiff")
-            return retry()
-        }
-        val sideDiff = (rect.width.toDouble() - rect.height).pow(2)
-        if (sideDiff > maxSideDiff.pow(2)) {
-            logger.debug("Estimate failed side difference test, will retry | diff=$sideDiff, max=$maxSideDiff")
-            return retry()
-        }
-        if (!window.contains(roi)) {
-            logger.info("Node $this not in map window")
-            val center = region.subRegion(
-                (region.width - 5) / 2,
-                (region.height - 5) / 2,
-                5, 5
+            // Rect that is relative to the window
+            val rect = h.transformRect(rect)
+            logger.debug("$this estimated to be at Rect(x=${rect.x}, y=${rect.y}, width=${rect.width}, height=${rect.height})")
+            if (rect.width <= 0 || rect.height <= 0) {
+                logger.debug("Estimate failed basic dimension test, will retry")
+                resetView()
+                continue
+            }
+            val roi = window.copyAs<AndroidRegion>(
+                window.x + rect.x,
+                window.y + rect.y,
+                rect.width,
+                rect.height
             )
-            // Add some randomness
-            center.translate(Random.nextInt(-50, 50), Random.nextInt(-50, 50))
-            when {
-                roi.y < window.y -> {
-                    val dist = max(window.y - roi.y, minScroll)
-                    val from = center.copyAs<AndroidRegion>().apply { translate(0, -dist) }
-                    val to = center.copyAs<AndroidRegion>().apply { translate(0, dist) }
-                    logger.info("Scroll up $dist px")
-                    from.swipeTo(to)
-                }
-                roi.y > window.y + window.height -> {
-                    val dist = max(roi.y - (window.y + window.height), minScroll)
-                    val from = center.copyAs<AndroidRegion>().apply { translate(0, dist) }
-                    val to = center.copyAs<AndroidRegion>().apply { translate(0, -dist) }
-                    logger.info("Scroll down $dist px")
-                    from.swipeTo(to)
-                }
+            val mapDiff = (rect.width.toDouble() - width).pow(2) + (rect.height.toDouble() - height).pow(2)
+            if (mapDiff > maxMapDiff.pow(2)) {
+                logger.info("Estimate failed map difference test, will retry | diff=$mapDiff, max=$maxMapDiff")
+                resetView()
+                continue
             }
-            when {
-                roi.x < window.x -> {
-                    val dist = max(window.x - roi.x, minScroll)
-                    val from = center.copyAs<AndroidRegion>().apply { translate(-dist, 0) }
-                    val to = center.copyAs<AndroidRegion>().apply { translate(dist, 0) }
-                    logger.info("Scroll left $dist px")
-                    from.swipeTo(to)
-                }
-                roi.x > window.x + window.width -> {
-                    val dist = max(roi.x - (window.x + window.width), minScroll)
-                    val from = center.copyAs<AndroidRegion>().apply { translate(dist, 0) }
-                    val to = center.copyAs<AndroidRegion>().apply { translate(-dist, 0) }
-                    logger.info("Scroll right $dist px")
-                    from.swipeTo(to)
-                }
+            val sideDiff = (rect.width.toDouble() - rect.height).pow(2)
+            if (sideDiff > maxSideDiff.pow(2)) {
+                logger.debug("Estimate failed side difference test, will retry | diff=$sideDiff, max=$maxSideDiff")
+                resetView()
+                continue
             }
-            mapH = null
-            delay(200)
-            return findRegion()
+            if (!window.contains(roi)) {
+                logger.info("Node $this not in map window")
+                val center = region.subRegion(
+                    (region.width - 5) / 2,
+                    (region.height - 5) / 2,
+                    5, 5
+                )
+                // Add some randomness
+                center.translate(Random.nextInt(-50, 50), Random.nextInt(-50, 50))
+                when {
+                    roi.y < window.y -> {
+                        val dist = max(window.y - roi.y, minScroll)
+                        val from = center.copyAs<AndroidRegion>().apply { translate(0, -dist) }
+                        val to = center.copyAs<AndroidRegion>().apply { translate(0, dist) }
+                        logger.info("Scroll up $dist px")
+                        from.swipeTo(to)
+                    }
+                    roi.y > window.y + window.height -> {
+                        val dist = max(roi.y - (window.y + window.height), minScroll)
+                        val from = center.copyAs<AndroidRegion>().apply { translate(0, dist) }
+                        val to = center.copyAs<AndroidRegion>().apply { translate(0, -dist) }
+                        logger.info("Scroll down $dist px")
+                        from.swipeTo(to)
+                    }
+                }
+                when {
+                    roi.x < window.x -> {
+                        val dist = max(window.x - roi.x, minScroll)
+                        val from = center.copyAs<AndroidRegion>().apply { translate(-dist, 0) }
+                        val to = center.copyAs<AndroidRegion>().apply { translate(dist, 0) }
+                        logger.info("Scroll left $dist px")
+                        from.swipeTo(to)
+                    }
+                    roi.x > window.x + window.width -> {
+                        val dist = max(roi.x - (window.x + window.width), minScroll)
+                        val from = center.copyAs<AndroidRegion>().apply { translate(dist, 0) }
+                        val to = center.copyAs<AndroidRegion>().apply { translate(-dist, 0) }
+                        logger.info("Scroll right $dist px")
+                        from.swipeTo(to)
+                    }
+                }
+                mapH = null
+                delay(200)
+                return findRegion()
+            }
+            return roi
         }
-        return roi
+        throw NodeNotFoundException(this)
     }
 }
