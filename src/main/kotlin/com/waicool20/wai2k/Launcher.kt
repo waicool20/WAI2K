c643a039954779ed4bcde764cce45b614c67b124
waicool20 <waicool20@gmail.com>
2021-05-20 11:27:08 +0800

    Fix empty stacktrace exception handler

--- src/main/kotlin/com/waicool20/wai2k/Launcher.kt
+++ src/main/kotlin/com/waicool20/wai2k/Launcher.kt
@@ -20,7 +20,71 @@
 package com.waicool20.wai2k
 
 import javafx.application.Application
+import javafx.application.Platform
+import javafx.scene.control.Alert
+import javafx.scene.control.Label
+import javafx.scene.control.TextArea
+import javafx.scene.layout.VBox
+import org.slf4j.LoggerFactory
+import tornadofx.*
 
 fun main(args: Array<String>) {
+    Thread.setDefaultUncaughtExceptionHandler(WAI2KExceptionHandler())
     Application.launch(Wai2K::class.java, *args)
+}
+
+/**
+ * Based off: https://github.com/edvin/tornadofx/blob/master/src/main/java/tornadofx/ErrorHandler.kt
+ * Fixes missing stack trace in text area
+ */
+class WAI2KExceptionHandler : Thread.UncaughtExceptionHandler {
+    val logger = LoggerFactory.getLogger("ErrorHandler")
+
+    class ErrorEvent(val thread: Thread, val error: Throwable) {
+        internal var consumed = false
+        fun consume() {
+            consumed = true
+        }
+    }
+
+    override fun uncaughtException(t: Thread, error: Throwable) {
+        logger.error("Uncaught error", error)
+        if (isCycle(error)) {
+            logger.info("Detected cycle handling error, aborting.", error)
+        } else {
+            val event = ErrorEvent(t, error)
+            if (!event.consumed) {
+                event.consume()
+                Platform.runLater { showErrorDialog(error) }
+            }
+        }
+    }
+
+    private fun isCycle(error: Throwable) = error.stackTrace.any {
+        it.className.startsWith("${javaClass.name}\$uncaughtException$")
+    }
+
+    private fun showErrorDialog(error: Throwable) {
+        val cause = Label(if (error.cause != null) error.cause?.message else "").apply {
+            style = "-fx-font-weight: bold"
+        }
+
+        val textarea = TextArea().apply {
+            prefRowCount = 20
+            prefColumnCount = 50
+            text = error.stackTraceToString()
+        }
+
+        Alert(Alert.AlertType.ERROR).apply {
+            title = error.message ?: "An error occured"
+            isResizable = true
+            headerText =
+                if (error.stackTrace.isNullOrEmpty()) "Error" else "Error in ${error.stackTrace[0]}"
+            dialogPane.content = VBox().apply {
+                add(cause)
+                add(textarea)
+            }
+            showAndWait()
+        }
+    }
 }
