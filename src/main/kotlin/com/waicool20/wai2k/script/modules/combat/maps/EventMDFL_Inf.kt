58fa6570a618e0c02a6f5fd1511f2a6624e128c2
waicool20 <waicool20@gmail.com>
2021-11-21 18:14:36 -0800

    Implement MDFL farm maps

--- src/main/kotlin/com/waicool20/wai2k/script/modules/combat/maps/EventMDFL_Inf.kt
+++ src/main/kotlin/com/waicool20/wai2k/script/modules/combat/maps/EventMDFL_Inf.kt
@@ -22,63 +22,80 @@ package com.waicool20.wai2k.script.modules.combat.maps
 import com.waicool20.wai2k.script.ScriptComponent
 import com.waicool20.wai2k.script.modules.combat.EventMapRunner
 import com.waicool20.wai2k.script.modules.combat.HomographyMapRunner
+import com.waicool20.wai2k.util.isSimilar
 import com.waicool20.waicoolutils.logging.loggerFor
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.yield
+import java.awt.Color
+import kotlin.math.roundToLong
 import kotlin.random.Random
 
-class EventBIAC_1_4(scriptComponent: ScriptComponent) : HomographyMapRunner(scriptComponent),
+abstract class EventMDFL_Inf(scriptComponent: ScriptComponent) :
+    HomographyMapRunner(scriptComponent),
     EventMapRunner {
-    private val logger = loggerFor<EventBIAC_1_4>()
+    private val logger = loggerFor<EventMapRunner>()
 
     override suspend fun enterMap() {
-        delay(2000)
-        region.pinch(
-            Random.nextInt(900, 1000),
-            Random.nextInt(300, 400),
-            15.0,
-            500
-        )
-        region.subRegion(1023, 405, 128, 21).click()
-        delay(300)
-        region.subRegion(1831, 589, 232, 111).click()
+        delay(3000) // Wait for the difficulty mode popup to settle
+        val capture = region.capture()
+        if (!Color(capture.getRGB(145, 960)).isSimilar(Color(238, 243, 238))) {
+            logger.info("Not on Normal mode, switching...")
+            region.subRegion(115, 947, 174, 61).click()
+        }
+
+        repeat(3) {
+            // Swipe from bottom left to top right
+            region.subRegion(570, 885, 40, 40)
+                .swipeTo(region.subRegion(1200, 440, 40, 40))
+        }
+
+        // Click on map pin
+        region.subRegion(1374, 339, 50, 50).click()
+        delay(500)
+
+        // Click on map entry
+        region.subRegion(1073, 514, 258, 29).click()
+        delay(500)
+
+        // Enter
+        region.subRegion(1832, 590, 232, 110).click()
     }
 
     override suspend fun begin() {
-        if (scriptRunner.justRestarted) {
+        if (gameState.requiresMapInit) {
+            logger.info("Zoom out")
             region.pinch(
-                Random.nextInt(700, 800),
+                Random.nextInt(900, 1000),
                 Random.nextInt(300, 400),
-                0.0,
+                15.0,
                 500
             )
-            delay(2000)
+            delay((900 * gameState.delayCoefficient).roundToLong()) //Wait to settle
             region.pinch(
-                Random.nextInt(300, 400),
-                Random.nextInt(400, 500),
-                0.0,
+                Random.nextInt(300, 350),
+                Random.nextInt(400, 450),
+                15.0,
                 500
             )
+            // Swipe up
+            val r1 = region.subRegion(605, 761, 50, 50)
+            val r2 = r1.copy(y = r1.y - 400)
+            r1.swipeTo(r2)
+            gameState.requiresMapInit = false
         }
         deployEchelons(nodes[0])
-        mapRunnerRegions.startOperation.click()
+        mapRunnerRegions.startOperation.click(); yield()
         waitForGNKSplash()
         resupplyEchelons(nodes[0])
+        delay(500)
         enterPlanningMode()
 
         logger.info("Selecting ${nodes[1]}")
         nodes[1].findRegion().click()
 
-        logger.info("Selecting ${nodes[2]}")
-        nodes[2].findRegion().click(); yield()
-
-        logger.info("Selecting ${nodes[3]}")
-        nodes[3].findRegion().click(); yield()
-
         logger.info("Executing plan")
         mapRunnerRegions.executePlan.click()
-
-        waitForTurnEnd(5)
+        waitForTurnEnd(4)
         handleBattleResults()
     }
 }
