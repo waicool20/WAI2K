8694c70b9662088e57146576258a38b14eb2a6a9
warezgibzzz <warzgibz@gmail.com>
2023-02-20 07:56:37 +0300

    Store plugin extraction

--- modules/ui/src/stores/plugins/ObserverPlugin.ts
+++ modules/ui/src/stores/plugins/ObserverPlugin.ts
@@ -1,10 +1,13 @@
 import type { PiniaPluginContext } from "pinia";
 
+const observableStores = ["profile", "config"];
 export function ObserverPlugin(context: PiniaPluginContext) {
   context.pinia.use(({ store }) => {
-    store.$subscribe(() => {
-      // react to store changes
-      console.log(store.$id);
-    });
+    if (observableStores.indexOf(store.$id) !== -1) {
+      store.$subscribe(() => {
+        // react to store changes
+        console.log(store.$id + " observed");
+      });
+    }
   });
 }
--- modules/ui/src/stores/plugins/injectApiPlugin.ts
+++ modules/ui/src/stores/plugins/injectApiPlugin.ts
@@ -1,23 +1,18 @@
 import type { AxiosStatic } from "axios";
 import type { PiniaPluginContext } from "pinia";
-import { inject } from "vue";
 
 declare module "pinia" {
   export interface PiniaCustomProperties {
-    $api: string | undefined;
-    axios: AxiosStatic | undefined;
+    $api: string;
+    axios: AxiosStatic;
   }
 }
 
 export function InjectApiPlugin(context: PiniaPluginContext) {
   context.pinia.use(({ app }) => {
-    const axios: AxiosStatic | undefined = app.config.globalProperties.axios;
-    const $api: string | undefined = app.config.globalProperties.$api;
-    console.log(axios);
-    console.log($api);
     return {
-      axios,
-      $api,
+      axios: app.config.globalProperties.axios,
+      $api: app.config.globalProperties.$api,
     };
   });
 }
